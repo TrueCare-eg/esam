878034623338d2e27b2cf000077f45c9149bcb6c
TrueCare-eg <esammamdouh.112211@gmail.com>
2024-09-27 06:37:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+﻿//scipt.js
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("slides");
+        let dots = document.getElementsByClassName("dots")[0].getElementsByTagName("span");
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+
+        slideIndex++;
+
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+
+        setTimeout(showSlides, 5000); // 
+    }
+
+    function plusSlides(n) {
+        slideIndex += n - 1;
+        showSlides();
+    }
+
+    function currentSlide(n) {
+        slideIndex = n;
+        showSlides();
+    }
+  function sendLocation() {
+      var lat = document.getElementById('latitude').value;
+      var lng = document.getElementById('longitude').value;
+      
+      if (lat && lng) {
+        var whatsappLink = "https://wa.me/201285421585?text=موقعي: https://www.google.com/maps?q=" + lat + "," + lng;
+        window.open(whatsappLink, "_blank");
+      } else {
+        alert("يرجى تحديد الموقع أولاً.");
+      }
+    }
